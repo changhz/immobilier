feffb7680a9b737d3bf60dad861d48b64882e1c9
ChangHZ <hz.chang@pm.me>
2020-01-12 22:09:16 -0500

    deploy

---
+++ precache-manifest.b516e6ea6992e5cba3c837a78d85666a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7539e0741785389f2537a0b37a7f62",
+    "url": "https://changhz.github.io/location/index.html"
+  },
+  {
+    "revision": "44270cb335d1da58d845",
+    "url": "https://changhz.github.io/location/static/css/main.1359096b.chunk.css"
+  },
+  {
+    "revision": "32fa12b0a23499bb25e3",
+    "url": "https://changhz.github.io/location/static/js/2.34a72993.chunk.js"
+  },
+  {
+    "revision": "c0243f2091d86bd8f4bb6e98d214b811",
+    "url": "https://changhz.github.io/location/static/js/2.34a72993.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44270cb335d1da58d845",
+    "url": "https://changhz.github.io/location/static/js/main.441c6623.chunk.js"
+  },
+  {
+    "revision": "f2f04ad544fd3281effa",
+    "url": "https://changhz.github.io/location/static/js/runtime-main.731e8e9d.js"
+  }
+]);
